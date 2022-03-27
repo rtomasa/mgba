e02bcc62a5795da59f3de73500000b2ceb1932b2
rtomasa <ruben.tomas.alonso@gmail.com>
2022-03-27 14:12:35 +0200

    Changed to SGB bezel

--- tools/snes-tile.py
+++ tools/snes-tile.py
@@ -1,6 +1,7 @@
 import numpy as np
 import PIL.Image
 import sys
+import struct
 
 X_FLIP = 0x4000
 Y_FLIP = 0x8000
@@ -64,13 +65,42 @@ for i, tile in enumerate(tiledata):
             tileData[2] |= ((t & 4) >> 2) << (7 - x)
             tileData[3] |= ((t & 8) >> 3) << (7 - x)
         tileBase = i * 32 + y * 2
-        print(tileBase)
+        #print(tileBase)
         chardataRaw[tileBase + 0x00] = tileData[0]
         chardataRaw[tileBase + 0x01] = tileData[1]
         chardataRaw[tileBase + 0x10] = tileData[2]
         chardataRaw[tileBase + 0x11] = tileData[3]
 
-def printMem(mem):
+def printMem(mem):  
     for i in range(len(mem) // 16):
         line = mem[i * 16:(i + 1) * 16]
         print(', '.join([f'0x{b:02X}' for b in line]) + ',')
+
+'''print('TILE MAP')
+print('='*8)
+printMem(tilemapRaw)
+print('CHAR DATA')
+print('='*9)
+printMem(chardataRaw)'''
+
+def to_argb1555(r, g, b, a):
+    r = (r >> 3) & 31
+    g = (g >> 3) & 31
+    b = (b >> 3) & 31
+    if (a == 0):
+        return 1 << 15
+    else:
+        return (r << 10) + (g << 5) + (b << 0)
+
+palette = set()
+
+for (count, color) in im.convert('RGBA').getcolors():
+    c = to_argb1555(*color)
+    c = "".join(struct.pack("<H", c))
+    palette.add(c)
+
+#palette = list(reversed(sorted(palette)))
+palette = list(palette)
+print("Used", len(palette), "colors out of 16.")
+print(palette)
+
