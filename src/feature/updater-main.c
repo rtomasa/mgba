382917110af28d2dbd4d4c8e059af9f7216a4bb4
Vicki Pfau <vi@endrift.com>
2022-05-30 20:38:44 -0700

    Updater: Add support for appimage

--- src/feature/updater-main.c
+++ src/feature/updater-main.c
@@ -92,21 +92,134 @@ int main(int argc, char* argv[]) {
 		puts("Cannot write to update path");
 	} else {
 		bool isPortable = mCoreConfigIsPortable();
-		struct VDir* archive = VDirOpenArchive(updateArchive);
-		if (!archive) {
-			puts("Cannot open update archive");
-		} else {
+		const char* extension = mUpdateGetArchiveExtension(&config);
+		struct VDir* archive = NULL;
+		bool prefix = true;
+		if (strcmp(extension, "dmg") == 0) {
+#ifdef __APPLE__
+			char mountpoint[PATH_MAX];
+			// Make a slightly random directory name for the updater mountpoint
+			struct timeval t;
+			gettimeofday(&t, NULL);
+			int printed = snprintf(mountpoint, sizeof(mountpoint), "/Volumes/%s Updater %04X", projectName, (t.tv_usec >> 2) & 0xFFFF);
+
+			// Fork hdiutil to mount it
+			char* args[] = {"hdiutil", "attach", "-nobrowse", "-mountpoint", mountpoint, updateArchive, NULL};
+			int fds[2];
+			pipe(fds);
+			pid_t pid = fork();
+			if (pid == 0) {
+				dup2(fds[1], STDOUT_FILENO);
+				execvp("hdiutil", args);
+				_exit(1);
+			} else {
+				// Parse out the disk ID so we can detach it when we're done
+				char buffer[1024] = {0};
+				ssize_t size;
+				while ((size = read(fds[0], buffer, sizeof(buffer) - 1)) > 0) { // Leave the last byte null
+					char* devinfo = strnstr(buffer, "\n/dev/disk", size);
+					if (!devinfo) {
+						continue;
+					}
+					char* devend = strpbrk(&devinfo[9], "s \t");
+					if (!devend) {
+						continue;
+					}
+					off_t diff = devend - devinfo - 1;
+					memcpy(devpath, &devinfo[1], diff);
+					puts(devpath);
+					break;
+				}
+				int retstat;
+				wait4(pid, &retstat, 0, NULL);
+			}
+			snprintf(&mountpoint[printed], sizeof(mountpoint) - printed, "/%s.app", projectName);
+			snprintf(subdir, sizeof(subdir), "%s/%s.app", root, projectName);
+			root = subdir;
+			archive = VDirOpen(mountpoint);
+			prefix = false;
+			needsUnmount = true;
+#endif
+		} else if (strcmp(extension, "appimage") != 0) {
+			archive = VDirOpenArchive(updateArchive);
+		}
+		if (archive) {
 			puts("Extracting update");
-			if (extractArchive(archive, root)) {
-				puts("Complete");
+			if (extractArchive(archive, root, prefix)) {
 				ok = 0;
-				mUpdateDeregister(&config);
 			} else {
 				puts("An error occurred");
 			}
 			archive->close(archive);
 			unlink(updateArchive);
 		}
+#ifdef __linux__
+		else if (strcmp(extension, "appimage") == 0) {
+			const char* command = mUpdateGetCommand(&config);
+			strlcpy(bin, command, sizeof(bin));
+			if (rename(updateArchive, bin) < 0) {
+				if (errno == EXDEV) {
+					// Cross-dev, need to copy manually
+					int infd = open(updateArchive, O_RDONLY);
+					int outfd = -1;
+					if (infd >= 0) {
+						ok = 2;
+					} else {
+						outfd = open(bin, O_CREAT | O_WRONLY | O_TRUNC, 0755);
+					}
+					if (outfd < 0) {
+						ok = 2;
+					} else {
+						uint8_t buffer[2048];
+						ssize_t size;
+						while ((size = read(infd, buffer, sizeof(buffer))) > 0) {
+							if (write(outfd, buffer, size) < size) {
+								ok = 2;
+								break;
+							}
+						}
+						if (size < 0) {
+							ok = 2;
+						}
+						close(outfd);
+						close(infd);
+					}
+					if (ok == 2) {
+						puts("Cannot move update over old file");
+					}
+				} else {
+					puts("Cannot move update over old file");
+				}
+			} else {
+				ok = 0;
+			}
+			if (ok == 0) {
+				chmod(bin, 0755);
+			}
+		}
+#endif
+		else {
+			puts("Cannot open update archive");
+		}
+		if (ok == 0) {
+			puts("Complete");
+			const char* command = mUpdateGetCommand(&config);
+			strlcpy(bin, command, sizeof(bin));
+			mUpdateDeregister(&config);
+		}
+#ifdef __APPLE__
+		if (needsUnmount) {
+			char* args[] = {"hdiutil", "detach", devpath, NULL};
+			pid_t pid = vfork();
+			if (pid == 0) {
+				execvp("hdiutil", args);
+				_exit(0);
+			} else {
+				int retstat;
+				wait4(pid, &retstat, 0, NULL);
+			}
+		}
+#endif
 		if (!isPortable) {
 			char portableIni[PATH_MAX] = {0};
 			snprintf(portableIni, sizeof(portableIni), "%s/portable.ini", root);
